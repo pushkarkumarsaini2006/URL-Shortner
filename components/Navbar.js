680c317e0c7fe31d05518410b58b1825093d9b32
Pushkar Kumar Saini <pushkarkumarsaini2006@gmail.com>
2025-08-13 20:28:14 +0530

    Remove About and Contact Us links from navigation bar

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,9 +9,7 @@ const Navbar = () => {
       </div>
       <ul className='flex justify-center gap-4 items-center'>
         <Link href="/"><li>Home</li></Link>
-        <Link href="/about"><li>About</li></Link>
         <Link href="/shorten"><li>Shorten</li></Link>
-        <Link href="/contact"><li>Contact Us</li></Link>
         <li className='flex gap-3'>
           <Link href="/shorten"><button className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>Try Now</button></Link>
           <Link href="https://github.com/pushkarkumarsaini2006/URL-Shortner" target="_blank"><button className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>GitHub</button></Link>
